9f010f59d468960befc6c7073309b85032df38d3
SoulBurn <47036679+SoulBurnAck@users.noreply.github.com>
2019-02-21 12:39:16 +0330

    Create SentryManager

---
+++ src/client/Helper/SentryManager.java
@@ -0,0 +1,21 @@
+package client.Helper;
+
+import client.model.Hero;
+
+public class SentryManager implements HeroManager {
+
+    @Override
+    public void preProcess() {
+
+    }
+
+    @Override
+    public void move() {
+
+    }
+
+    @Override
+    public void takeAction(Hero currentHero) {
+
+    }
+}
